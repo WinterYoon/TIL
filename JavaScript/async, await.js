1471f5e5f2baa8dc2d74e07b88a21136a0468382
user1 <ritz7taste@gmail.com>
2021-11-04 23:59:40 +0900

    자바스크립트

--- JavaScript/async, await.js
+++ JavaScript/async, await.js
@@ -21,3 +21,8 @@ async function func1(){
     }
 }
 fun1();
+
+
+// promise 와의 비교
+// async는 선언만 해도 promise 를 사용할 수 있다. await는 promise가 처리될 때까지 대기하고, 콜백함수 없이 비동기 처리가 가능하다.
+// 비동기 작업을 수행하고자 하는 함수 앞에 async를 표기하고 함수 내부에서 실질적인 비동기 작업이 필요한 위치마다 await 를 표기하여 뒤의 내용을 promise로 전환시키고 해당 내용이 resolve 된 이후에 다음으로 진행한다. 
